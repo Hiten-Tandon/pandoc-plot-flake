74470f7f8db6173b7009b5ac687f734350a171e2
Hiten Tandon <tandonhiten@gamil.com>
2025-02-16 04:35:24 +0530

    Feat: Stuff

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pandoc Plot Flake
+This is the pandoc plot filter available as a flake.
+The original project is avaialable at [https://github.com/LaurentRDC/pandoc-plot](https://github.com/LaurentRDC/pandoc-plot)
---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "A very basic flake";
+
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+
+  outputs = { nixpkgs, ... }:
+    with import nixpkgs { system = "x86_64-linux"; }; 
+    let
+      system = "x86_64-linux";
+      version = "1.9.1";
+      application = fetchzip {
+        url = "https://github.com/LaurentRDC/pandoc-plot/releases/download/${version}/pandoc-plot-Linux-x86_64-static.zip";
+        sha256 = "PWWiZOwscpAR9b7t1oCh/+FevgQ/0Z1se9f4XjenP+c=";
+      };
+
+    in {
+      packages.${system}.default = stdenv.mkDerivation (finalAttrs: {
+        pname = "pandoc-plot";
+        inherit version;
+      
+        src = application;
+      
+        nativeBuildInputs = [
+          gmp
+          libz
+          unzip
+        ];
+        
+        phases = ["unpackPhase" "installPhase" "fixupPhase"];
+        installPhase = ''
+          mkdir -p $out/bin
+          cp $src/pandoc-plot $out/bin/pandoc-plot
+        '';
+        fixupPhase = ''
+          chmod +xw $out/bin/pandoc-plot
+          patchelf --set-rpath ${gmp}/lib:${libz}/lib $out/bin/pandoc-plot
+          chmod -w $out/bin/pandoc-plot
+        '';
+      
+        meta = {
+          description = "Render and include figures in Pandoc documents using your plotting toolkit of choice";
+          homepage = "https://laurentrdc.github.io/pandoc-plot/";
+          license = lib.licenses.gpl2;
+          maintainers = with lib.maintainers; [ LaurentRDC mgajda SanchayanMaity ];
+        };
+        
+      });
+      formatter.${system} = nixfmt-classic;
+    };
+}
